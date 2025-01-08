f458420f05cbe82095cddcc81c8e8554134e72f6
aimanyusufw <aimanyusufdev@gmail.com>
2025-01-08 19:30:47 +0700

    add<resorce> : add Advertisment

---
+++ database/migrations/2025_01_07_083829_create_advertisements_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('type');
+            $table->string('media_path')->nullable();
+            $table->string('redirect_url')->nullable();
+            $table->string('location');
+            $table->string('dimensions')->nullable();
+            $table->integer('priority')->default(0);
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->integer('max_impressions')->nullable();
+            $table->integer('max_views_per_user')->nullable();
+            $table->bigInteger('clicks')->default(0);
+            $table->bigInteger('impressions')->default(0);
+            $table->string('advertiser_name')->nullable();
+            $table->string('advertiser_email')->nullable();
+            $table->string('advertiser_phone')->nullable();
+            $table->enum('status', ['active', 'inactive', 'pending'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advertisements');
+    }
+};

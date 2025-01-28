86f3a50946136d08ffb21d614c17256d33afa31c
aimanyusufw <aimanyusufdev@gmail.com>
2025-01-28 21:32:53 +0700

    add : seed for category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use function Laravel\Prompts\form;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,5 +26,21 @@ class DatabaseSeeder extends Seeder
 
         $user = User::create(["name" => "Admin", "email" => "admin@admin.com", "password" => "12345678"]);
         $user->assignRole("super_admin");
+
+        $categories =  [
+            "Politics",
+            "World",
+            "Finance",
+            "Sport",
+            "Technology"
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                "name" => $category,
+                "slug" => Str::slug($category),
+                "description" => "Post fot category " . $category
+            ]);
+        }
     }
 }

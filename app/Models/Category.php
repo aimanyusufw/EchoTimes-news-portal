e2be8acde8167e50b5da2aed41f347feb36706bf
aimanyusufw <aimanyusufdev@gmail.com>
2025-01-28 21:33:05 +0700

    ref : route key to slug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,11 @@ class Category extends Model
 
     protected $fillable = ["name", "slug", "description"];
 
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
+
     /** 
      * Relation with sub category model
      * Category has many sub category

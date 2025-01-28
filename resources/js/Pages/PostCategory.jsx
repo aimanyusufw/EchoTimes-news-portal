9a5fa5301a04c763b6eb06899685e4d0eeae264e
aimanyusufw <aimanyusufdev@gmail.com>
2025-01-28 21:33:55 +0700

    add : setup category post page

---
+++ resources/js/Pages/PostCategory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Post = ({ category }) => {
+    return (
+        <div>
+            <h1>Post {category.name} Page</h1>
+        </div>
+    );
+};
+
+export default Post;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia("Home");
+    return inertia("Home", [
+        "categories" => Category::all()
+    ]);
+});
+
+Route::get('/category/{category}', function (Category $category) {
+    return inertia("PostCategory", ["category" => $category]);
 });
